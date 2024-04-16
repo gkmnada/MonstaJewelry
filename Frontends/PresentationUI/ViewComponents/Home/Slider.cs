ac7f24b3ad79272b5a2aabfbc291d846665e2dbe
gkmnada <gkmenada@hotmail.com>
2024-04-17 01:23:05 +0300

    MONSTA

---
+++ Frontends/PresentationUI/ViewComponents/Home/Slider.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationUI.ViewComponents.Home
+{
+    public class Slider : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/PresentationUI/ViewComponents/Layout/Footer.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationUI.ViewComponents.Layout
+{
+    public class Footer : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
