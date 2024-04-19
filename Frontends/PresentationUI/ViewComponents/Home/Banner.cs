fc2692883495db4c1745bb70cf0ece9c30a9b725
gkmnada <gkmenada@hotmail.com>
2024-04-19 12:07:41 +0300

    MONSTA

---
+++ Frontends/PresentationUI/ViewComponents/Home/Banner.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationUI.ViewComponents.Home
+{
+    public class Banner : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/PresentationUI/ViewComponents/Home/BestSellers.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationUI.ViewComponents.Home
+{
+    public class BestSellers : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
